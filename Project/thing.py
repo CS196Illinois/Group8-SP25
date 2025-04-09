673a53f2a6f01e0eacfd58d311847eb25182ae36
Asrith Sunke <asrith@wirelessprv-10-195-251-96.near.illinois.edu>
2025-04-09 17:26:50 -0500

    vader sub

--- Project/thing.py
+++ Project/thing.py
@@ -1,59 +1,45 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch.nn.functional as F
-from transformers import pipeline
-import numpy as np
-from transformers import DebertaV2Tokenizer
+# from transformers import AutoTokenizer, AutoModelForSequenceClassification
+# import torch.nn.functional as F
+# from transformers import pipeline
+# import numpy as np
+# from transformers import DebertaV2Tokenizer
 
 
-# absa_tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
-absa_tokenizer = DebertaV2Tokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1", use_fast=False)
-absa_model = AutoModelForSequenceClassification \
-  .from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+# # absa_tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+# absa_tokenizer = DebertaV2Tokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1", use_fast=False)
+# absa_model = AutoModelForSequenceClassification \
+#   .from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+
+# def sentimeter(sentence, aspect):
+#   inputs = absa_tokenizer(f"[CLS] {sentence} [SEP] {aspect} [SEP]", return_tensors="pt")
+#   outputs = absa_model(**inputs)
+#   probs = F.softmax(outputs.logits, dim=1)
+#   probs = probs.detach().numpy()[0]
+#   max_index = np.argmax(probs)
+#   max_value = probs[max_index]
+#   labels = ["negative", "neutral", "positive"]
+#   max_label = labels[max_index]
+#   return max_value, max_label 
+#   #return probs[0], probs[1], probs[2] # -- this does all 3 probs which are in order of negative, neutral, positive.
+
+# print(sentimeter("I got the phone", "phone"))
+# #"I got the phone", "phone"
+# #"I really liked the phone but I really hated how the screen was", "screen" or "phone"
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+vader_analyzer = SentimentIntensityAnalyzer()
+
+def sentimeter(sentence):
+    scores = vader_analyzer.polarity_scores(sentence)
+    compound = scores['compound']
+    if compound >= 0.05:
+        label = 'positive'
+    elif compound <= -0.05:
+        label = 'negative'
+    else:
+        label = 'neutral'
+    
+    return compound, label
 
 
-sentiment_model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
-sentiment_model = pipeline("sentiment-analysis", model=sentiment_model_path,
-                          tokenizer=sentiment_model_path)
-
-# sentence = "We had a great experience at the restaurant, food was delicious, but " \
-#   "the service was kinda bad"
-# print(f"Sentence: {sentence}")
-# print()
-
-# aspect = "food"
-# inputs = absa_tokenizer(f"[CLS] {sentence} [SEP] {aspect} [SEP]", return_tensors="pt")
-# outputs = absa_model(**inputs)
-# probs = F.softmax(outputs.logits, dim=1)
-# probs = probs.detach().numpy()[0]
-# print(f"Sentiment of aspect '{aspect}' is:")
-# for prob, label in zip(probs, ["negative", "neutral", "positive"]):
-#   print(f"Label {label}: {prob}")
-# print()
-
-
-# aspect = "service"
-# inputs = absa_tokenizer(f"[CLS] {sentence} [SEP] {aspect} [SEP]", return_tensors="pt")
-# outputs = absa_model(**inputs)
-# probs = F.softmax(outputs.logits, dim=1)
-# probs = probs.detach().numpy()[0]
-# print(f"Sentiment of aspect '{aspect}' is:")
-# for prob, label in zip(probs, ["negative", "neutral", "positive"]):
-#   print(f"Label {label}: {prob}")
-# print()
-
-# sentiment = sentiment_model([sentence])[0]
-# print(f"Overall sentiment: {sentiment['label']} with score {sentiment['score']}")
-
-def sentimeter(sentence, aspect):
-  inputs = absa_tokenizer(f"[CLS] {sentence} [SEP] {aspect} [SEP]", return_tensors="pt")
-  outputs = absa_model(**inputs)
-  probs = F.softmax(outputs.logits, dim=1)
-  probs = probs.detach().numpy()[0]
-  max_index = np.argmax(probs)
-  max_value = probs[max_index]
-  labels = ["negative", "neutral", "positive"]
-  max_label = labels[max_index]
-  #return max_value, max_label 
-  return probs[0], probs[1], probs[2] # -- this does all 3 probs which are in order of negative, neutiral, positive.
-
-print(sentimeter("This product was decent and mid", "product"))
+print(sentimeter("Solid phone for the price!"))
